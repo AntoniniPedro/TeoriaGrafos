270b30f0e143ecccdfd3450dc4e2741aaebc0732
Pedro Antonini <p.antoninigomes@gmail.com>
2023-06-12 20:28:43 -0300

    entrega do trabalho

---
+++ main.js
@@ -0,0 +1,314 @@
+const data ={
+  "Belo Horizonte": {
+    "Belo Horizonte": 0,
+    "Contagem": 19,
+    "Uberlândia": 536,
+    "Juiz de fora": 261,
+    "Ribeirão das Neves": 33,
+    "Betim": 29,
+    "Montes Claros": 425,
+    "Uberaba": 476,
+    "Governador Valadares": 322,
+    "Santa Luzia": 25,
+    "Ipatinga": 223,
+    "Sete Lagoas": 75,
+    "Divinopolis": 117,
+    "Poços de Caldas": 462,
+    "Ibirité": 21
+  },
+  "Contagem": {
+    "Belo Horizonte": 19,
+    "Contagem": 0,
+    "Uberlândia": 523,
+    "Juiz de fora": 271,
+    "Ribeirão das Neves": 28,
+    "Betim": 17,
+    "Montes Claros": 415,
+    "Uberaba": 463,
+    "Governador Valadares": 336,
+    "Santa Luzia": 39,
+    "Ipatinga": 236,
+    "Sete Lagoas": 63,
+    "Divinopolis": 104,
+    "Poços de Caldas": 449,
+    "Ibirité": 19
+  },
+  "Uberlândia": {
+    "Belo Horizonte": 536,
+    "Contagem": 523,
+    "Uberlândia": 0,
+    "Juiz de fora": 787,
+    "Ribeirão das Neves": 552,
+    "Betim": 506,
+    "Montes Claros": 627,
+    "Uberaba": 106,
+    "Governador Valadares": 857,
+    "Santa Luzia": 561,
+    "Ipatinga": 758,
+    "Sete Lagoas": 597,
+    "Divinopolis": 458,
+    "Poços de Caldas": 474,
+    "Ibirité": 525
+  },
+  "Juiz de fora": {
+    "Belo Horizonte": 261,
+    "Contagem": 270,
+    "Uberlândia": 785,
+    "Juiz de fora": 0,
+    "Ribeirão das Neves": 291,
+    "Betim": 279,
+    "Montes Claros": 677,
+    "Uberaba": 726,
+    "Governador Valadares": 455,
+    "Santa Luzia": 284,
+    "Ipatinga": 428,
+    "Sete Lagoas": 325,
+    "Divinopolis": 322,
+    "Poços de Caldas": 458,
+    "Ibirité": 267
+  },
+  "Ribeirão das Neves": {
+    "Belo Horizonte": 33,
+    "Contagem": 28,
+    "Uberlândia": 552,
+    "Juiz de fora": 291,
+    "Ribeirão das Neves": 0,
+    "Betim": 46,
+    "Montes Claros": 398,
+    "Uberaba": 491,
+    "Governador Valadares": 334,
+    "Santa Luzia": 33,
+    "Ipatinga": 235,
+    "Sete Lagoas": 46,
+    "Divinopolis": 168,
+    "Poços de Caldas": 478,
+    "Ibirité": 44
+  },
+  "Betim": {
+    "Belo Horizonte": 29,
+    "Contagem": 17,
+    "Uberlândia": 506,
+    "Juiz de fora": 279,
+    "Ribeirão das Neves": 41,
+    "Betim": 0,
+    "Montes Claros": 433,
+    "Uberaba": 445,
+    "Governador Valadares": 345,
+    "Santa Luzia": 52,
+    "Ipatinga": 245,
+    "Sete Lagoas": 80,
+    "Divinopolis": 121,
+    "Poços de Caldas": 434,
+    "Ibirité": 22
+  },
+  "Montes Claros": {
+    "Belo Horizonte": 425,
+    "Contagem": 415,
+    "Uberlândia": 627,
+    "Juiz de fora": 677,
+    "Ribeirão das Neves": 399,
+    "Betim": 432,
+    "Montes Claros": 0,
+    "Uberaba": 648,
+    "Governador Valadares": 535,
+    "Santa Luzia": 433,
+    "Ipatinga": 523,
+    "Sete Lagoas": 360,
+    "Divinopolis": 554,
+    "Poços de Caldas": 865,
+    "Ibirité": 430
+  },
+  "Uberaba": {
+    "Belo Horizonte": 476,
+    "Contagem": 463,
+    "Uberlândia": 106,
+    "Juiz de fora": 726,
+    "Ribeirão das Neves": 488,
+    "Betim": 446,
+    "Montes Claros": 648,
+    "Uberaba": 0,
+    "Governador Valadares": 791,
+    "Santa Luzia": 498,
+    "Ipatinga": 692,
+    "Sete Lagoas": 521,
+    "Divinopolis": 398,
+    "Poços de Caldas": 368,
+    "Ibirité": 465
+  },
+  "Governador Valadares": {
+    "Belo Horizonte": 322,
+    "Contagem": 336,
+    "Uberlândia": 857,
+    "Juiz de fora": 455,
+    "Ribeirão das Neves": 339,
+    "Betim": 351,
+    "Montes Claros": 534,
+    "Uberaba": 796,
+    "Governador Valadares": 0,
+    "Santa Luzia": 313,
+    "Ipatinga": 102,
+    "Sete Lagoas": 380,
+    "Divinopolis": 473,
+    "Poços de Caldas": 783,
+    "Ibirité": 340
+  },
+  "Santa Luzia": {
+    "Belo Horizonte": 25,
+    "Contagem": 39,
+    "Uberlândia": 561,
+    "Juiz de fora": 284,
+    "Ribeirão das Neves": 33,
+    "Betim": 54,
+    "Montes Claros": 429,
+    "Uberaba": 500,
+    "Governador Valadares": 310,
+    "Santa Luzia": 0,
+    "Ipatinga": 210,
+    "Sete Lagoas": 70,
+    "Divinopolis": 176,
+    "Poços de Caldas": 487,
+    "Ibirité": 43
+  },
+  "Ipatinga": {
+    "Belo Horizonte": 223,
+    "Contagem": 236,
+    "Uberlândia": 758,
+    "Juiz de fora": 428,
+    "Ribeirão das Neves": 240,
+    "Betim": 251,
+    "Montes Claros": 525,
+    "Uberaba": 697,
+    "Governador Valadares": 103,
+    "Santa Luzia": 214,
+    "Ipatinga": 0,
+    "Sete Lagoas": 281,
+    "Divinopolis": 373,
+    "Poços de Caldas": 684,
+    "Ibirité": 240
+  },
+  "Sete Lagoas": {
+    "Belo Horizonte": 75,
+    "Contagem": 63,
+    "Uberlândia": 597,
+    "Juiz de fora": 325,
+    "Ribeirão das Neves": 47,
+    "Betim": 80,
+    "Montes Claros": 360,
+    "Uberaba": 520,
+    "Governador Valadares": 377,
+    "Santa Luzia": 73,
+    "Ipatinga": 277,
+    "Sete Lagoas": 0,
+    "Divinopolis": 202,
+    "Poços de Caldas": 513,
+    "Ibirité": 78
+  },
+  "Divinopolis": {
+    "Belo Horizonte": 117,
+    "Contagem": 104,
+    "Uberlândia": 458,
+    "Juiz de fora": 322,
+    "Ribeirão das Neves": 129,
+    "Betim": 87,
+    "Montes Claros": 520,
+    "Uberaba": 397,
+    "Governador Valadares": 433,
+    "Santa Luzia": 140,
+    "Ipatinga": 333,
+    "Sete Lagoas": 168,
+    "Divinopolis": 0,
+    "Poços de Caldas": 390,
+    "Ibirité": 106
+  },
+  "Poços de Caldas": {
+    "Belo Horizonte": 462,
+    "Contagem": 449,
+    "Uberlândia": 474,
+    "Juiz de fora": 458,
+    "Ribeirão das Neves": 473,
+    "Betim": 434,
+    "Montes Claros": 865,
+    "Uberaba": 366,
+    "Governador Valadares": 777,
+    "Santa Luzia": 484,
+    "Ipatinga": 677,
+    "Sete Lagoas": 512,
+    "Divinopolis": 390,
+    "Poços de Caldas": 0,
+    "Ibirité": 450
+  },
+  "Ibirité": {
+    "Belo Horizonte": 21,
+    "Contagem": 19,
+    "Uberlândia": 525,
+    "Juiz de fora": 267,
+    "Ribeirão das Neves": 39,
+    "Betim": 22,
+    "Montes Claros": 430,
+    "Uberaba": 464,
+    "Governador Valadares": 336,
+    "Santa Luzia": 43,
+    "Ipatinga": 236,
+    "Sete Lagoas": 77,
+    "Divinopolis": 140,
+    "Poços de Caldas": 451,
+    "Ibirité": 0
+  },
+  
+  
+};
+
+function nearestNeighborAlgorithm() {
+  const startCity = document.getElementById("startCity").value;
+
+  const cities = Object.keys(data); // Obter a lista de cidades
+  const numCities = cities.length; // Número total de cidades
+  const visitedCities = [startCity]; // Lista de cidades visitadas
+  let currentCity = startCity; // Cidade atual
+
+  let total = 0;
+
+  while (visitedCities.length < numCities) {
+    let minDistance = Infinity; // Distância mínima inicialmente infinita
+    let nearestCity = null;
+
+    // Percorrer todas as cidades para encontrar a cidade mais próxima
+    for (let city of cities) {
+      if (!visitedCities.includes(city)) {
+        const distance = data[currentCity][city];
+        if (distance < minDistance) {
+          
+          if (total == 0) {
+            total = distance;
+          }
+
+          minDistance = distance;
+          nearestCity = city;
+        }
+      }
+    }
+
+    visitedCities.push(nearestCity); // Adicionar a cidade mais próxima à lista de cidades visitadas
+    currentCity = nearestCity; // Atualizar a cidade atual
+  }
+
+  visitedCities.push(startCity); // Adicionar a cidade de partida novamente para formar um circuito
+
+  const totalDistance = visitedCities.reduce(
+    (acc, city, index) => {
+
+      if(index < visitedCities.length - 1) {
+        total += data[city][visitedCities[index + 1]];
+        lastCity = city;
+      } else {
+        return;
+      }
+
+    }
+  ); // Calcular a distância total percorrida
+
+  console.log("Cidades visitadas:", visitedCities.join(" -> "));
+  console.log("Distância total percorrida:", total, "km");
+
+  //return { visitedCities, totalDistance };
+}
